1851e17675ce274d19956fe52cea2eaf55390f1b
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2020-04-07 15:04:58 +0200

    TST: Add tests for __getitem__, __setitem__, __delitem__

--- tests/test_sparray.py
+++ tests/test_sparray.py
@@ -17,3 +17,25 @@ def test_init__different_default_value__should_set_for_all_elements():
     assert x[0, 2] == 7
     assert x[1, 1] == 7
 
+
+def test_getitem_setitem__indices_within_shape__should_work_correctly():
+    A = sp.sparray((3, 3))
+
+    A[0, 0] = 10.0
+    assert A[0, 0] == 10.0
+
+    A[2, 1] = 47.98
+    assert A[2, 1] == 47.98
+
+    A[0, 2] = -1.32
+    assert A[0, 2] == -1.32
+
+
+def test_delitem__indices_within_shape__should_work_correctly():
+    s = sp.sparray((25, 14))
+
+    s[5, 2] = 87.5
+
+    del s[5, 2]
+
+    assert s[5, 2] == 0.0
