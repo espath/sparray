445c7d95ae1f25a4884f833b5b26d7c6f602f88a
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2020-04-07 15:04:37 +0200

    TST: Add test for the default value

--- tests/test_sparray.py
+++ tests/test_sparray.py
@@ -10,4 +10,10 @@ def test_init__default_arguments__should_set_properties_correctly():
     assert s.dtype == float
 
 
+def test_init__different_default_value__should_set_for_all_elements():
+    x = sp.sparray((3, 3), default=7)
+    
+    assert x[2, 2] == 7
+    assert x[0, 2] == 7
+    assert x[1, 1] == 7
 
